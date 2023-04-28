6bfd92fdb303942d6e0c2c98ad7a7b3ae5ae1a33
omdglobalia <omd.globaliasoft@gmail.com>
2023-04-28 11:08:31 +0530

    core modules

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs = require('fs'); // As requirement we import module according us! Like - [ const fs = require('fs').writeFile ]
+const fs_specific = require('fs').writeFileSync;
+
+fs_specific("file.txt", "This is file is created with spacifically imported module")
 
 fs.writeFile('mynewfile.txt', "helloo", function (err) {
     if (err) {
