f5327aa96ba36fbfcaf30d0ad9b9033b4939bb17
omdglobalia <omd.globaliasoft@gmail.com>
2023-04-28 14:42:29 +0530

    create and delete file from cmd input

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const http = require('http');
-const data = require('./data')
+const fs = require('fs');
 
-http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type0": "application/json" })
-    res.write(JSON.stringify(data))
-    res.end();
-}).listen(8000);
+// For enter input pass input in comand line like - [
+//     - node index.js add test.txt "Hello I'm from cmd"
+//     - node index.js remove test.txt
+// ]
+const input = process.argv
+
+if (input[2] === "add") {
+    fs.writeFileSync(input[3], input[4]);
+} else if (input[2] === "remove") {
+    fs.unlinkSync(input[3]);
+} else {
+    console.log("Input Invalid")
+}
