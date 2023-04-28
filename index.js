731346a7e1b102fb34bc600b4bf17cf283bd1424
omdglobalia <omd.globaliasoft@gmail.com>
2023-04-28 16:09:12 +0530

    crud with file system

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname, 'files');
+const dirPath = path.join(__dirname, 'crud');
+const filePath = `${dirPath}/test.txt`;
 
-// for (i = 0; i < 5; i++) {
-//     fs.writeFileSync(`${dirPath}/hello${i}.txt`, `Hello I'm from hello${i}, How can i help you?`)
-// }
+// CREATE FILE
+// fs.writeFileSync(filePath, `This is a simple text file`)
 
-fs.readdir(dirPath, (err, files) => {
-    files.forEach((item) => {
-        console.log(item)
-    })
-})
+// READ FILE
+// fs.readFile(filePath, 'utf8', (err, item) => {
+//     console.log(item)
+// })
+
+// UPDATE FILE
+// fs.appendFile(filePath, " and file name is hello.txt", (err) => !err && console.log("file is updated"))
+
+// RENAME FILE
+// fs.rename(filePath, `${dirPath}/test.txt`, (err) => !err && console.log("file name is updated"))
+
+// DELETE FILE
+// fs.unlinkSync(filePath)
