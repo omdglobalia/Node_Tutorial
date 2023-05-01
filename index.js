64a4b5e6e1090dd3faca56038a79084b5710a610
omdglobalia <omd.globaliasoft@gmail.com>
2023-05-01 15:22:11 +0530

    render HTML code in browser

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('', (req, res) => {
-    res.send("Hello this is a home page")
-})
+app.get("", (req, res) => {
+    res.send(`
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+    `);
+});
 
-app.get('/about', (req, res) => {
-    res.send("Hello this is a about page")
-})
+app.get("/about", (req, res) => {
+    res.send(`
+    <h1 style="background-color:powderblue;">This is a about page</h1>
+    <p style="background-color:tomato;">This is a paragraph.</p>
+    `);
+});
 
-app.get('/help', (req, res) => {
-    res.send("Hello this is a help page")
-})
+app.get("/help", (req, res) => {
+    res.send(`
+    <p>My favorite color is <del>blue</del> red.</p>
+    `);
+});
 
 app.listen(4500);
