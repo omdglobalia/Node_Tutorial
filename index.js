3089b4c90918aaba19173652bbe3b4718619a668
omdglobalia <omd.globaliasoft@gmail.com>
2023-05-01 17:26:46 +0530

    rander html pages

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 const express = require("express");
+const path = require('path');
+
 const app = express();
+const publicPath = path.join(__dirname, 'public');
 
-app.get("", (req, res) => {
-    res.send(`
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>
-    `);
-});
-
-app.get("/about", (req, res) => {
-    res.send(`
-    <h1 style="background-color:powderblue;">This is a about page</h1>
-    <p style="background-color:tomato;">This is a paragraph.</p>
-    `);
-});
-
-app.get("/help", (req, res) => {
-    res.send(`
-    <p>My favorite color is <del>blue</del> red.</p>
-    `);
-});
+app.use(express.static(publicPath));
 
 app.listen(4500);
