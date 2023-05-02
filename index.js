f48f899d1f517ff46eca63eeaeec48bb9288f0c0
omdglobalia <omd.globaliasoft@gmail.com>
2023-05-02 10:50:26 +0530

    remove file extensions and added 404 page

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+
+app.get('', (req, res) => {
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(`${publicPath}/404.html`)
+})
 
 app.listen(4500);
