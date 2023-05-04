9fe6bac4ad42b21f62ba798f8eecbb7bf1ebe3c4
omdglobalia <omd.globaliasoft@gmail.com>
2023-05-04 09:26:48 +0530

    create dynamic page

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
-// app.use(express.static(publicPath));
+app.set('view engine', 'ejs');
 
 app.get('', (req, res) => {
     res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/profile', (req, res) => {
+
+    const user = {
+        name: "Om Davra",
+        img: {
+            src: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+            alt: "Photo of user"
+        },
+        redirect: "/about",
+        skills: [
+            "React Js",
+            "Next Js",
+            "Node Js",
+            "Express Js",
+            "Redux",
+        ]
+    }
+
+    res.render('profile', { user })
 })
 
 app.get('/about', (req, res) => {
